d705263d21b140820cdddcfa174efad2354decf7
jing <45102664+jingl12@users.noreply.github.com>
2018-11-17 14:55:44 +0800

    Create chengfab.py

---
+++ chengfab.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: jingl12
+# @Date:   2018-11-17 14:45:59
+
+'''
+Python实现，九九乘法表
+'''
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('%d*%d=%d'%(j,i,i*j),end=' ')
+    print()
